0b3496fb4305144e04e97ccfc36b31d2cc711751
nhovay8 <nhovay8@gmail.com>
2019-12-07 18:27:02 +0700

    Implement Rest API

--- src/main/java/com/quan/HomeController.java
+++ src/main/java/com/quan/HomeController.java
@@ -1,62 +1,29 @@
 package com.quan;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.quan.service.*;
+import com.quan.service.Alien;
+import com.quan.service.AlienService;
 
-@Controller
+@RestController
 public class HomeController {
 	
 	@Autowired(required = true)
 	AlienService alienService;
 	
-	@RequestMapping("/")
-	public String home() {
-		return "home.jsp";
+	@RequestMapping("/aliens")
+	public Iterable<Alien>  getAliens() {
+		Iterable<Alien> alien = alienService.findAll();
+		return alien; // return data instead of view name as in MVC
 	}
 	
-	@RequestMapping("/addAlien")
-	public String  addAlien(@ModelAttribute Alien alien) {
-		
-		Alien a = alienService.save(alien);
-		return "home.jsp";
-	}
-	
-	@RequestMapping("/getAlien")
-	public ModelAndView  getAlien(@RequestParam int aid) {
-		ModelAndView modelAndView = new ModelAndView("showAlien.jsp");
-		Alien alien = alienService.findById(aid).orElse(new Alien());
-		modelAndView.addObject(alien);
-		return modelAndView;
-	}
-	
-	@RequestMapping("/getByTech")
-	public String getByTech() {
-		return "getAlienByTech.jsp";
-	}
-	
-	@RequestMapping("/getAlienByTech")
-	public ModelAndView  getAlienByTech(@RequestParam String tech) {
-		ModelAndView modelAndView = new ModelAndView("showListAlien.jsp");
-		
-		//find by Tech property
-		List<Alien> aliens = alienService.findByTech(tech);
-		System.out.println(aliens);
-		
-		//find by Tech but sorted
-		List<Alien> findByNameSorted = alienService.findByNameSorted(tech);
-		System.out.println(findByNameSorted);
-		
-		modelAndView.addObject(aliens);
-		return modelAndView;
+	@RequestMapping("/alien/{aid}")
+	public Alien  getAlien(@PathVariable int aid) {
+		Alien alien = alienService.findById(aid).orElse(null);
+		return alien; // return data instead of view name as in MVC
 	}
 
 }
